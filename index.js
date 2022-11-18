a0e7e4a3adbf77e75df5375e05817e8229067744
Caio Lopes <caiolopeis95@gmail.com>
2022-11-17 21:18:10 -0300

    Servidor configurado e criado

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("olá,caio")
+const express = require('express');
+const servidor = express();
+// Definir a public como pasta de arquivos estaticos
+servidor.use(express.static("public"));
+// Definir rota
+servidor.get(
+    '/produtos',
+    (req, res) => {
+    return res.sendFile(__dirname + '/views/produtos.html');
+}
+)
+servidor.listen(3000);
